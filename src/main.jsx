c18f98b0e7bb6cf1c9d2483e84e7f5103a231311
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-02-20 14:42:13 +0100

    fix github blank page issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 <React.StrictMode>
