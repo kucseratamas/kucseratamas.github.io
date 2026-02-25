c08d6f9c8e640cb4937a3792d1ef103d0dbfc9bf
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-02-25 01:05:59 +0100

    Update Block with scalability and remove hover setting

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,9 +1,11 @@
-// Block.jsx
 import React from 'react';
 import './Block.css';
 
-const Block = ({ children, className = "" }) => {
-  return <div className={`glass ${className}`}>{children}</div>;
+const Block = ({ children, className = "", width, height  }) => {
+  return <div className={`glass ${className}`} style={{
+        width: width || "260px",
+        height: height || "180px",
+      }}>{children}</div>;
 };
 
 export default Block;
