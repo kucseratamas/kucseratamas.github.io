6412b469778c16bac1d4b326abb012ae0db6b11f
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-02-25 01:10:08 +0100

    Update About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,15 @@
-export default function About() {
-  return <h1>About</h1>;
+import React from "react";
+import '../components/About.css'
+import Block from "../components/Block";
+
+const About = () => {
+  return(
+    <div className="about">
+        <Block>
+            <h1></h1>
+            <p> </p>
+        </Block>
+    </div>
+  )
 }
+export default About
