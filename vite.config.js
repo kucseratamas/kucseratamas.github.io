57ecfcb7c860c9d308fb62431f0c7765fb705204
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-02-25 01:07:35 +0100

    Add three.js config for 3D object

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: '/',
-    build: {
-    outDir: 'dist',
-  }
+
+  assetsInclude: ["**/*.glb", "**/*.gltf"],
+
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
 });
