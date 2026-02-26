7fa42faeb264a186d9f8cabef2162df97b11f08c
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-02-26 20:23:29 +0100

    Delete font size from Block

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Block.css';
 
-const Block = ({ children, className = "", width, height  }) => {
+const Block = ({ children, className = "", width, height,  }) => {
   return <div className={`glass ${className}`} style={{
         width: width || "260px",
         height: height || "180px",
