5a6e7728b3a1eeab250aa6911f6d2af096b9f5d8
DESKTOP-V7SOJ4H\Tomi <kucsitomika@gmail.com>
2026-03-02 16:24:17 +0100

    Fix Block fix width and height bug

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Block.css';
 
-const Block = ({ children, className = "", width, height,  }) => {
-  return <div className={`glass ${className}`} style={{
-        width: width || "260px",
-        height: height || "180px",
-      }}>{children}</div>;
+const Block = ({ children, className = "", width, height }) => {
+  return (
+    <div
+      className={`glass ${className}`}
+      style={{
+        width: width || "100%",
+        height: height || "auto"
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Block;
